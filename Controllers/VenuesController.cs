bd95a16d3623de5fc3b51784207377485f71130a
Kiaanporsche <ST10441363@vcconnect.edu.za>
2025-05-13 17:48:46 +0200

    last commit

--- Controllers/VenuesController.cs
+++ Controllers/VenuesController.cs
@@ -64,7 +64,7 @@ namespace EventEaseCloud.Controllers
             //Date: 6 May 2025
             //Code cersion: 1
             //Availability: https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/sorting-filtering-and-paging-with-the-entity-framework-in-an-asp-net-mvc-application 
-
+            
         }
 
         // GET: Venues/Details/5
